04ffe8e07abf5a215163a661cb814ce20fd04808
zdz <1392777056@qq.com>
2019-10-10 11:05:10 +0800

    redis： 一致性 穿透性 雪崩

---
+++ src/main/java/cn/concurrency/demo/cache/CaCheController.java
@@ -0,0 +1,30 @@
+package cn.concurrency.demo.cache;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/cache")
+public class CaCheController {
+
+    @Autowired
+    private RedisClient redisClient;
+
+    @RequestMapping("/set")
+    @ResponseBody
+    public String set(@RequestParam("k") String k,@RequestParam("v") String v) throws Exception{
+        redisClient.set(k,v);
+        return "SECCESS";
+    }
+
+    @RequestMapping("/get")
+    @ResponseBody
+    public String get(@RequestParam("k") String k) throws Exception{
+
+        return redisClient.get(k);
+    }
+
+}
